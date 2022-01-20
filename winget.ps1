53f2cd04147ee85ae84859529469f3c0de151bdd
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2022-01-20 10:38:05 +0000

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,36 @@
+##############################
+# install
+##############################
+
+# install gclould sdk
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe", "$env:Temp\GoogleCloudSDKInstaller.exe")
+& $env:Temp\GoogleCloudSDKInstaller.exe
+
+# apps
+winget install -e --id Google.Chrome
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id MongoDB.Compass.Full
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Microsoft.WindowsTerminal
+
+winget install -e --id Git.Git
+winget install -e --id OpenJS.NodeJS
+# TODO nvm https://github.com/coreybutler/nvm-windows/releases
+winget install -e --id GoLang.Go
+# winget install -e --id Microsoft.VisualStudio.2019.Community
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id Microsoft.dotnet
+
+# install wsl
+wsl --install
+wsl --install --distribution Ubuntu
+
+##############################
+# configure
+##############################
+# change ubuntu to run on wsl 1. For some reason wsl2 is not sharing network 
+wsl --set-version Ubuntu 1
+
+git config --global user.name "Luiz Felipe" 
+git config --global user.email "lfmachadodasilva@gmail.com"
+git config --global core.editor "vim"
