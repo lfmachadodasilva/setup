4c927f7b76117aa214e3d611986c4b9dc4144f8a
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2022-02-08 06:17:27 +0000

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -21,21 +21,26 @@ brew install --cask \
 echo ">>>> installing general packages"
 brew install --cask zoom Google-chrome firefox spotify 
 
+echo ">>>> cleaning up brew"
+brew cleanup 
+
 if [[ $SHELL != *"zsh" ]]; then
-    echo "Installing zsh and setting it as default shell"
+    echo ">>>> installing zsh and setting it as default shell"
     brew install zsh
     chsh -s /usr/local/bin/zsh
 fi
 
 if test ! $(whence p10k); then
-    echo "Installing/Updating Powerlevel10k"
+    echo ">>>> installing/Updating Powerlevel10k"
     curl -fsSL https://raw.githubusercontent.com/romkatv/powerlevel10k/master/config/p10k-lean.zsh -o ~/.p10k.zsh
     echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then\n  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"\nfi\nPOWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true\nsource /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme\n[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
     brew install romkatv/powerlevel10k/powerlevel10k
 fi
 
-echo ">>>> cleaning up"
-brew cleanup
+if test ! $(whence k); then
+    echo ">>>> setting alias k = kubectl"
+    echo "alias k='kubectl'" >>~/.zshrc
+fi
 
 echo ">>>> configure git"
 git config --global user.name "Luiz Felipe" 
